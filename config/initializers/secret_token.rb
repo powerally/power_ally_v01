444a1e039bd8844b1855b57eadffb10b4ad66d71
B R <brbryanr@gmail.com>
2012-08-02 15:52:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerAllyV01::Application.config.secret_token = 'ef40b0be3e839c39ce94976a7b0e8127a8c35e8683c378512fd7bffa11bf18e4f635694e50debe69842e44ff0f7a28107e9a956db2570cf96ebcf84d648d067a'
